0510c84a3754e4bb279b074dab8d3cd20d9a6ff8
unknown <miladkhoshdel@gmail.com>
2020-01-20 20:46:19 +0330

    Add Network Interface Section

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ console.log("UpTime: " + (os.uptime()/3600).toFixed(2) + " hour(s)");
 console.log("CPU Model: " + os.cpus()[0].model);
 console.log("Total Memory: " + (os.totalmem()/1073741824).toFixed(2) +  " GB");
 console.log("Free Memory: " + (os.freemem()/1073741824).toFixed(2) + " GB");
-console.log("Network Interfaces: " + Object.keys(os.networkInterfaces()).length )
+console.log("[+] Network Interfaces: " + Object.keys(os.networkInterfaces()).length )
+for (let i=0; i<Object.keys(os.networkInterfaces()).length ; i++){
+    console.log(" [-] Name: " + Object.keys(os.networkInterfaces())[i]);
+    for(let y=0; y<Object.values(os.networkInterfaces())[i].length; y++){
+        console.log("\t" + "IP" + y + ": " + Object.values(os.networkInterfaces())[i][y].address);
+    }
+}
